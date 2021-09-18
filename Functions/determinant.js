bcc96426e5c6cc1a54b359e63318baf946a2b0c2
blazarus11 <benjamin.lazarus11@gmail.com>
2021-09-18 19:01:10 -0400

    table

--- Functions/determinant.js
+++ Functions/determinant.js
@@ -49,3 +49,33 @@ let mountains = [
   let data = Object.keys(mountains[0]);
   generateTableHead(table, data);
   generateTable(table, mountains);
+
+
+var A = [ [1,2,3,4],
+          [5,6,7,8],
+          [9,1,2,3], 
+          [4,5,9,7] ];
+
+function det(A) {
+  if(A.length() <= 1) {
+    return (A[0][0]*A[1][1])-(A[0][1]-A[1][0]);
+  }
+
+  var answer = 0;
+  for(var i = 0; i < A.length; i++) {
+    answer = Math.pow(-1,i)*A[0][i]*det(reduce(A, i));
+  }
+  return answer;
+}
+function reduce(A, index) {
+
+  var min = [];
+  for (var i = 0; i < A.length; i++) {
+    min.push(A[i].slice(0));
+  }
+  min.splice(0,1);
+  for (var i = 0; i < temp.length; i++) {
+    return min
+  }
+}
+document.getElementById('answer').innerHTML = det(M);
