0f2359366654ba4df7c010d561b61ca0ae4616f6
qingyuan-wu <qyw.wu@mail.utoronto.ca>
2021-09-18 17:22:01 -0400

    added table.js, table.html

--- Functions/table.js
+++ Functions/table.js
@@ -1,20 +1,7 @@
+
 document.getElementById("submit").addEventListener("click", storeDimension);
-
-function storeDimension() {
-    const input1 = document.getElementById("userInput1").value;
-    const input2 = document.getElementById("userInput2").value;
-    alert(`Rows: ${input1}\nCols: ${input2}`);
-    for (var i = 0; i < input1; i++) {
-        var row = table.insertRow(i);
-        for (var j = 0; j < input2; j++) {
-            row.inserCell(i);
-            cell1.innerHTML = "some text"; //Put "some text" in all column1 fields
-            cell2.innerHTML = '<input type="text" class="form-control" size="5" maxlength="4" />'; //Make all fields in column2 input fields.
-        }
-    }
-    return input1, input2;
-}
-
+//things to figure out:
+//addEventListener
 let mountains = [
     { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
     { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
@@ -35,6 +22,9 @@ let mountains = [
   }
   
   function generateTable(table, data) {
+    const input1 = document.getElementById("userInput1").value;
+    const input2 = document.getElementById("userInput2").value;
+
     for (let element of data) {
       let row = table.insertRow();
       for (key in element) {
